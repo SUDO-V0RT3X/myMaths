0fca4aadb81d4c6066287fdd49f4d07984628b14
Venom Vortex <family.osman786@gmail.com>
2021-09-20 16:28:44 +0200

    initial commit. the start of something great B)

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+const xA = parseFloat(document.getElementById("xA"))
+const xB = parseFloat(document.getElementById("xB"))
+const yA = parseFloat(document.getElementById("yA"))
+const yB = parseFloat(document.getElementById("yB"))
+
+const preAnsOne = parseFloat((xA - xB) * (xA - xB))
+*/
+
+document.getElementById("answer").textContent = "new text"
+
+console.log(preAnsOne)
