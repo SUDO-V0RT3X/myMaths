3832f17ccf4dc88f7b09cf880b6e2790c886938e
Venom Vortex <family.osman786@gmail.com>
2021-09-24 19:15:33 +0200

    having problems...2nd commit

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/*
-const xA = parseFloat(document.getElementById("xA"))
-const xB = parseFloat(document.getElementById("xB"))
-const yA = parseFloat(document.getElementById("yA"))
-const yB = parseFloat(document.getElementById("yB"))
-
-const preAnsOne = parseFloat((xA - xB) * (xA - xB))
-*/
-
-document.getElementById("answer").textContent = "new text"
-
-console.log(preAnsOne)
+const xA = document.getElementById("xA")
+function answer() {
+  console.log("hello")
+  document.getElementById("answerr").innerHTML = xA
+}
