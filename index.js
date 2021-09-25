9a87ca580d8846ab28c4c29ab98f0245830fac46
Venom Vortex <family.osman786@gmail.com>
2021-09-25 10:39:08 +0200

    gui updated a little

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function answer() {
-  var aX = parseFloat(document.getElementById("aX").value)
-  var aY = parseFloat(document.getElementById("aY").value)
-  var bX = parseFloat(document.getElementById("bX").value)
-  var bY = parseFloat(document.getElementById("bY").value)
+    var aX = parseFloat(document.getElementById("aX").value)
+    var aY = parseFloat(document.getElementById("aY").value)
+    var bX = parseFloat(document.getElementById("bX").value)
+    var bY = parseFloat(document.getElementById("bY").value)
 
-  var answerField = document.getElementById("answer-field")
-  var answer = parseFloat(
-    Math.sqrt((bY - aY) * (bY - aY) + (bX - aX) * (bX - aX))
-  )
+    var answerField = document.getElementById("answer-field")
+    var answer = parseFloat(
+        Math.sqrt((bY - aY) * (bY - aY) + (bX - aX) * (bX - aX))
+    )
 
-  console.log(answer)
-  answerField.innerHTML = answer
+    console.log(answer)
+    answerField.innerHTML = answer
 }
