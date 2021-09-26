54dcf14c35522a11066dcfb31ab0cccf20c91b01
Venom Vortex <family.osman786@gmail.com>
2021-09-26 16:31:13 +0200

    midpoint formula working. 2 algorithms done - hope to introduce a bunch more tomorrow after my physics test

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-function answer() {
-    var aX = parseFloat(document.getElementById("aX").value)
-    var aY = parseFloat(document.getElementById("aY").value)
-    var bX = parseFloat(document.getElementById("bX").value)
-    var bY = parseFloat(document.getElementById("bY").value)
+function distance() {
+    var aX = parseFloat(document.getElementById("daX").value)
+    var aY = parseFloat(document.getElementById("daY").value)
+    var bX = parseFloat(document.getElementById("dbX").value)
+    var bY = parseFloat(document.getElementById("dbY").value)
 
-    var answerField = document.getElementById("answer-field")
+    var answerField = document.getElementById("answer-field-d")
     var answer = parseFloat(
         Math.sqrt((bY - aY) * (bY - aY) + (bX - aX) * (bX - aX))
     )
 
     console.log(answer)
     answerField.innerHTML = answer
+}
+
+function midpoint() {
+    var aX = parseFloat(document.getElementById("maX").value)
+    var aY = parseFloat(document.getElementById("maY").value)
+    var bX = parseFloat(document.getElementById("mbX").value)
+    var bY = parseFloat(document.getElementById("mbY").value)
+
+    var answerField = document.getElementById("answer-field-m")
+    var answerx = parseFloat((aX + bX) / 2)
+    var answery = parseFloat((aY + bY) / 2)
+
+
+
+    console.log(answerx + " ; " + answery)
+    answerField.innerHTML = answerx + " ; " + answery
 }
