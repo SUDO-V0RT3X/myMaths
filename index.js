94faf186d896db2032fd7757bfbb618a4b27d4bb
Venom Vortex <family.osman786@gmail.com>
2021-09-30 13:37:45 +0200

    fixing bugsssss

--- index.js
+++ index.js
@@ -1,101 +1,88 @@
 function distance() {
-  var aX = parseFloat(document.getElementById("daX").value)
-  var aY = parseFloat(document.getElementById("daY").value)
-  var bX = parseFloat(document.getElementById("dbX").value)
-  var bY = parseFloat(document.getElementById("dbY").value)
+    var aX = parseFloat(document.getElementById("daX").value)
+    var aY = parseFloat(document.getElementById("daY").value)
+    var bX = parseFloat(document.getElementById("dbX").value)
+    var bY = parseFloat(document.getElementById("dbY").value)
 
-  var answerField = document.getElementById("answer-field-d")
-  var answer = parseFloat(
-    Math.sqrt((bY - aY) * (bY - aY) + (bX - aX) * (bX - aX))
-  )
-  var ansOne =
-    "&#8730" +
-    "(Xb - Xa)" +
-    "<sup>2</sup>" +
-    " " +
-    "+" +
-    " " +
-    "(Yb - Ya)" +
-    "<sup>2</sup>"
+    var answerField = document.getElementById("answer-field-d")
+    var answer = parseFloat(
+        Math.sqrt((bX - aX) * (bX - aX) + (bY - aY) * (bY - aY))
+    )
 
-  var ansTwo =
-    "&#8730" +
-    "(" +
-    bX +
-    "-" +
-    aX +
-    ")" +
-    "<sup>2</sup>" +
-    " " +
-    "+" +
-    "(" +
-    bY +
-    "-" +
-    aY +
-    ")" +
-    "<sup>2</sup>"
+    var anss = ((bX - aX) * (bX - aX))
+    var ansss = ((bY - aY) * (bY - aY))
+    var anssss = toString(anss + ansss)
 
-  var ansThree =
-    "&#8730" +
-    "(" +
-    (bX - aX) +
-    ")" +
-    "<sup>2</sup>" +
-    "+" +
-    "(" +
-    (bY - aY) +
-    ")" +
-    "<sup>2</sup>"
+    var ansOne =
+        "&#8730" +
+        "(Xb - Xa)" +
+        "<sup>2</sup>" +
+        " " +
+        "+" +
+        " " +
+        "(Yb - Ya)" +
+        "<sup>2</sup>"
 
-  var ansFour =
-    "&#8730" +
-    "(" +
-    (bX - aX) * (bX - aX) +
-    ")" +
-    " + " +
-    "(" +
-    (bY - aY) * (bY - aY) +
-    ")"
+    var ansTwo =
+        "&#8730" +
+        "(" +
+        bX +
+        "-" +
+        aX +
+        ")" +
+        "<sup>2</sup>" +
+        " " +
+        "+" +
+        "(" +
+        bY +
+        "-" +
+        aY +
+        ")" +
+        "<sup>2</sup>"
 
-  /*fix ansFive :
+    var ansThree =
+        "&#8730" +
+        "(" +
+        (bX - aX) + ")" +
+        "<sup>2</sup>" +
+        "+" + "(" +
+        (bY - aY) + ")" +
+        "<sup>2</sup>"
 
-  -var for (bx - ax) squared
-  -same for y values 
-  -problem: above is being converted to string and appended to each ohter instead of being calculated
-  */
+    var ansFour =
+        "&#8730" +
+        anss +
+        " + " +
+        ansss
 
-  var ansFive =
-    "&#8730" +
-    "(" +
-    parseFloat((bX - aX) * (bX - aX)) +
-    parseFloat((bY - aY) * (bY - aY)) +
-    ")"
+    var ansFive = "&#8730" + anssss
 
-  console.log(ansOne + ansTwo + ansThree)
-  answerField.innerHTML =
-    ansOne +
-    "</br>" +
-    ansTwo +
-    "</br>" +
-    ansThree +
-    "</br>" +
-    ansFour +
-    "</br>" +
-    ansFive
+    console.log(ansOne + ansTwo + ansThree + ansFour + ansFive + answer)
+    answerField.innerHTML =
+        ansOne +
+        "</br>" +
+        ansTwo +
+        "</br>" +
+        ansThree +
+        "</br>" +
+        ansFour +
+        "</br>" +
+        ansFive +
+        "</br>" +
+        answer
 
-  //answerField.innerHTML = answer
+    //answerField.innerHTML = answer
 }
 
 function midpoint() {
-  var aX = parseFloat(document.getElementById("maX").value)
-  var aY = parseFloat(document.getElementById("maY").value)
-  var bX = parseFloat(document.getElementById("mbX").value)
-  var bY = parseFloat(document.getElementById("mbY").value)
+    var aX = parseFloat(document.getElementById("maX").value)
+    var aY = parseFloat(document.getElementById("maY").value)
+    var bX = parseFloat(document.getElementById("mbX").value)
+    var bY = parseFloat(document.getElementById("mbY").value)
 
-  var answerField = document.getElementById("answer-field-m")
-  var answerx = parseFloat((aX + bX) / 2)
-  var answery = parseFloat((aY + bY) / 2)
+    var answerField = document.getElementById("answer-field-m")
+    var answerx = parseFloat((aX + bX) / 2)
+    var answery = parseFloat((aY + bY) / 2)
 
-  console.log(answerx + " ; " + answery)
-  answerField.innerHTML = answerx + " ; " + answery
+    console.log(answerx + " ; " + answery)
 }
