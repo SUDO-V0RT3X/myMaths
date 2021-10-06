3c63f46ed3f753902398d469402579eedfb3856b
Venom Vortex <family.osman786@gmail.com>
2021-10-06 20:38:11 +0200

    fixed some major stuff ... all the three main algorithms done, ui will be tackled next. good progress (:

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ function distance() {
     var aY = parseFloat(document.getElementById("daY").value)
     var bX = parseFloat(document.getElementById("dbX").value)
     var bY = parseFloat(document.getElementById("dbY").value)
-
     var answerField = document.getElementById("answer-field-d")
-    var answer = parseFloat(
+
+    var doneX = ((bX - aX) * (bX - aX))
+    var doneY = ((bY - aY) * (bY - aY))
+    var doneA = doneX + doneY
+    var answerF = parseFloat(
         Math.sqrt((bX - aX) * (bX - aX) + (bY - aY) * (bY - aY))
     )
-
-    var anss = ((bX - aX) * (bX - aX))
-    var ansss = ((bY - aY) * (bY - aY))
-    var anssss = toString(anss + ansss)
 
     var ansOne =
         "&#8730" +
@@ -51,16 +50,16 @@ function distance() {
 
     var ansFour =
         "&#8730" +
-        anss +
+        "&nbsp;" +
+        doneX +
         " + " +
-        ansss
+        doneY
 
-    var ansFive = "&#8730" + anssss
+    var ansFive = "&#8730" + "&nbsp;" + doneA
 
-    console.log(ansOne + ansTwo + ansThree + ansFour + ansFive + answer)
     answerField.innerHTML =
         ansOne +
-        "</br>" +
+        "<br>" +
         ansTwo +
         "</br>" +
         ansThree +
@@ -69,9 +68,9 @@ function distance() {
         "</br>" +
         ansFive +
         "</br>" +
-        answer
+        answerF
 
-    //answerField.innerHTML = answer
+    // console.log(ansOne, '\n', ansTwo, '\n', ansThree, '\n', ansFour, '\n', ansFive)
 }
 
 function midpoint() {
@@ -81,8 +80,39 @@ function midpoint() {
     var bY = parseFloat(document.getElementById("mbY").value)
 
     var answerField = document.getElementById("answer-field-m")
-    var answerx = parseFloat((aX + bX) / 2)
-    var answery = parseFloat((aY + bY) / 2)
+    var answerX = parseFloat(aX + bX)
+    var answerY = parseFloat(aY + bY)
+    var answerx = parseFloat(answerX / 2)
+    var answery = parseFloat(answerY / 2)
 
-    console.log(answerx + " ; " + answery)
+    var ansOne = "(" + "Xa + Xb / 2 " + " ; " + " Ya + Yb / 2 " + ")"
+    var ansTwo = "(" + answerX + " /2 " + " ; " + answerY + " /2 " + ")"
+    var ansThree = "(" + answerx + " ; " + answery + ")"
+
+    answerField.innerHTML = ansOne + "<br>" + ansTwo + "<br>" + ansThree + "<br>"
+
+    //console.log(answerx + " ; " + answery)
+}
+
+
+function gradient() {
+    var aX = parseFloat(document.getElementById("gaX").value)
+    var aY = parseFloat(document.getElementById("gaY").value)
+    var bX = parseFloat(document.getElementById("gbX").value)
+    var bY = parseFloat(document.getElementById("gbY").value)
+
+    var answerField = document.getElementById("answer-field-g")
+    var answerX = parseFloat(bX - aX)
+    var answerY = parseFloat(bY - aY)
+    var answerF = parseFloat(answerY / answerX)
+
+
+    var ansOne = "Yb - Ya " + " / " + "Xb- Xa"
+    var ansTwo = bY + "-" + aY + " / " + bX + "-" + aX
+    var ansThree = answerY + " / " + answerX
+    var ansFour = answerF
+
+    answerField.innerHTML = ansOne + "<br>" + ansTwo + "<br>" + ansThree + "<br>" + ansFour
+
+    //console.log(ansOne, '\n', ansTwo, '\n', ansThree, '\n', ansFour)
 }
